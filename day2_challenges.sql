3f3ff327634ae182b5cc2a8e9e78a9aca97b7522
Vignesh waran <vigneshwaransaravanan123@gmail.com>
2026-03-20 22:03:43 +0530

    Add files via upload

---
+++ day2_challenges.sql
@@ -0,0 +1,7 @@
+-- Challenges
+
+-- Find students scoring exactly 75
+SELECT name FROM students WHERE marks = 75;
+
+-- Find students not in Madurai
+SELECT name FROM students WHERE city != 'Madurai';
